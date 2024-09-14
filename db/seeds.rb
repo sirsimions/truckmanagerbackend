b3099d1d53366b0265f4cb064ff3239a9d6d7455
Simion Okola <okola.simion168@gmail.com>
2024-09-14 13:07:51 +0300

    passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,28 +11,29 @@
   # { truck: 'Truck 2', customer: 'Customer B', product: 'Product Y', depot: 'Depot 2', route_name: 'Mombasa - Jinja', date_loaded: '2024-08-02', mode: 'Tanker', loaded_status: 'Empty', location: 'Mombasa', status_date: '2024-08-02', status_time: '16:00', status: 'Empty', remarks: 'Delayed', distance_covered: 200, region: 'Coast', country: 'Kenya' }
 # ])
 
+
+# db/seeds.rb
+
 Author.create([
-  { name: 'suhara logistics', department: 'transport', password: 'suhara123'},
-  { name: 'suhara', department: 'admin1', password: 'suhara123#', email: 'okola.simion168@gmail.com' },
-  { name: 'suhara logistics', department: 'transport', password: 'suhara123', email: 'jane@gmail.com' },
-  { name: 'Shadab', department: 'transport', password: 'shadab123', email: 'jane@gmail.com' },
-  { name: 'Nityanand', department: 'transport', password: 'nitya123', email: 'jane@gmail.com' },
-  { name: 'Kuntal', department: 'transport', password: 'kuntal123', email: 'jane@gmail.com' },
-  { name: 'Angila', department: 'transport', password: 'angilal123', email: 'jane@gmail.com' },
-  { name: 'Julia', department: 'transport', password: 'julia123', email: 'jane@gmail.com' },
-  { name: 'Wilson', department: 'transport', password: 'wilson123', email: 'jane@gmail.com' },
-  { name: 'Obadiah', department: 'transport', password: 'obadiah123', email: 'jane@gmail.com' },
-  { name: 'Faith M', department: 'transport', password: 'faithm123', email: 'jane@gmail.com' },
-  { name: 'Bonface', department: 'transport', password: 'bonface123', email: 'jane@gmail.com' },
-  { name: 'Sunil', department: 'transport', password: 'sunil123', email: 'jane@gmail.com' },
-  { name: 'Leo', department: 'transport', password: 'leo123', email: 'jane@gmail.com' },
-  { name: 'Saifali', department: 'transport', password: 'saifali123', email: 'jane@gmail.com' },
-  { name: 'Gatabazi', department: 'transport', password: 'gatabazi123', email: 'jane@gmail.com' },
-  { name: 'Rodney', department: 'transport', password: 'rodney123', email: 'jane@gmail.com' },
-  { name: 'Bidget', department: 'transport', password: 'bidget123', email: 'jane@gmail.com' },
-  { name: 'Veronica', department: 'transport', password: 'veronica123', email: 'jane@gmail.com' },
-  { name: 'Simion', department: 'transport', password: 'simion123', email: 'jane@gmail.com' }
-  { name: 'Simion', department: 'transport', password: 'Arafat123', email: 'jane@gmail.com' }
-
+  { name: 'suhara logistics', department: 'transport', password: ENV['DEFAULT_PASSWORD'] },
+  { name: 'suhara', department: 'admin1', password: ENV['DEFAULT_PASSWORD'], email: 'okola.s168@gmail.com' },
+  { name: 'suhara logistics', department: 'transport', password: ENV['DEFAULT_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Shadab', department: 'transport', password: ENV['SHADAB_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Nityanand', department: 'transport', password: ENV['NITYANAND_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Kuntal', department: 'transport', password: ENV['KUNTAL_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Angila', department: 'transport', password: ENV['ANGILA_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Julia', department: 'transport', password: ENV['JULIA_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Wilson', department: 'transport', password: ENV['WILSON_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Obadiah', department: 'transport', password: ENV['OBADIAH_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Faith M', department: 'transport', password: ENV['FAITHM_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Bonface', department: 'transport', password: ENV['BONFACE_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Sunil', department: 'transport', password: ENV['SUNIL_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Leo', department: 'transport', password: ENV['LEO_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Saifali', department: 'transport', password: ENV['SAIFALI_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Gatabazi', department: 'transport', password: ENV['GATABAZI_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Rodney', department: 'transport', password: ENV['RODNEY_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Bidget', department: 'transport', password: ENV['BIDGET_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Veronica', department: 'transport', password: ENV['VERONICA_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Simion', department: 'transport', password: ENV['SIMION_PASSWORD'], email: 'jane@gmail.com' },
+  { name: 'Arafat', department: 'transport', password: ENV['ARAFAT_PASSWORD'], email: 'jane@gmail.com' }
 ])
-
